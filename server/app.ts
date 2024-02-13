c9cc0c4412acaa0ed376bb7029cb23b14629fad6
shahidmhd <shahidvk124@gmail.com>
2024-02-13 14:27:10 +0530

    initial commit

---
+++ server/app.ts
@@ -0,0 +1,37 @@
+import express, { NextFunction, Request, Response } from "express";
+export const app = express();
+import cors from "cors";
+import cookieparser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
+// body parser
+app.use(express.json({ limit: "50mb" }));
+
+// cookie parser
+app.use(cookieparser());
+
+// cors
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+// test api
+
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "api is working",
+  });
+});
+
+
+// unknown route
+
+app.all("*",(req:Request,res:Response,next:NextFunction)=>{
+    const err=new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statuscode=404;
+    next(err)
+})
+
+app.use(ErrorMiddleware);
